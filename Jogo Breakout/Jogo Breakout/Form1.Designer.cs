4bb6642699523b6f2d60567b3b8cbf8d0525e40f
Leandro Semedo <158284651+LeandroSemedo99@users.noreply.github.com>
2024-01-31 12:46:51 +0000

    Add files via upload

---
+++ Jogo Breakout/Jogo Breakout/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Jogo_Breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.score = new System.Windows.Forms.Label();
+            this.relogio = new System.Windows.Forms.Timer(this.components);
+            this.Bola = new System.Windows.Forms.PictureBox();
+            this.jogador = new System.Windows.Forms.PictureBox();
+            this.bDjogoDataSet = new Jogo_Breakout.BDjogoDataSet();
+            this.pontuaçõesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pontuaçõesTableAdapter = new Jogo_Breakout.BDjogoDataSetTableAdapters.PontuaçõesTableAdapter();
+            this.tableAdapterManager = new Jogo_Breakout.BDjogoDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.Bola)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDjogoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pontuaçõesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Felix Titling", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.score.Location = new System.Drawing.Point(9, 14);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(156, 23);
+            this.score.TabIndex = 0;
+            this.score.Text = "Pontuação:0";
+            // 
+            // relogio
+            // 
+            this.relogio.Interval = 20;
+            this.relogio.Tick += new System.EventHandler(this.mainrelogioevent);
+            // 
+            // Bola
+            // 
+            this.Bola.BackColor = System.Drawing.Color.Cyan;
+            this.Bola.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Bola.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Bola.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Bola.Image = global::Jogo_Breakout.Properties.Resources.Smile;
+            this.Bola.Location = new System.Drawing.Point(709, 538);
+            this.Bola.Name = "Bola";
+            this.Bola.Size = new System.Drawing.Size(33, 31);
+            this.Bola.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Bola.TabIndex = 1;
+            this.Bola.TabStop = false;
+            // 
+            // jogador
+            // 
+            this.jogador.BackColor = System.Drawing.Color.White;
+            this.jogador.BackgroundImage = global::Jogo_Breakout.Properties.Resources.Jogador;
+            this.jogador.Location = new System.Drawing.Point(639, 590);
+            this.jogador.Name = "jogador";
+            this.jogador.Size = new System.Drawing.Size(170, 57);
+            this.jogador.TabIndex = 1;
+            this.jogador.TabStop = false;
+            // 
+            // bDjogoDataSet
+            // 
+            this.bDjogoDataSet.DataSetName = "BDjogoDataSet";
+            this.bDjogoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pontuaçõesBindingSource
+            // 
+            this.pontuaçõesBindingSource.DataMember = "Pontuações";
+            this.pontuaçõesBindingSource.DataSource = this.bDjogoDataSet;
+            this.pontuaçõesBindingSource.CurrentChanged += new System.EventHandler(this.pontuaçõesBindingSource_CurrentChanged);
+            // 
+            // pontuaçõesTableAdapter
+            // 
+            this.pontuaçõesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PontuaçõesTableAdapter = this.pontuaçõesTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Jogo_Breakout.BDjogoDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.Bola);
+            this.Controls.Add(this.jogador);
+            this.Controls.Add(this.score);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Jogo Breakout";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Bola)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jogador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDjogoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pontuaçõesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.PictureBox jogador;
+        private System.Windows.Forms.PictureBox Bola;
+        private System.Windows.Forms.Timer relogio;
+        private BDjogoDataSet bDjogoDataSet;
+        private System.Windows.Forms.BindingSource pontuaçõesBindingSource;
+        private BDjogoDataSetTableAdapters.PontuaçõesTableAdapter pontuaçõesTableAdapter;
+        private BDjogoDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+    }
+}
+
---
+++ Jogo Breakout/Jogo Breakout/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Jogo_Breakout
+{
+    public partial class Form1 : Form
+    {
+        bool irdrt;
+        bool iresq;
+        bool isGameOver;
+
+        int pontuação;
+        int bolax;
+        int bolay;
+        int velocidade;
+        WindowsMediaPlayer musica = new WindowsMediaPlayer();
+        WindowsMediaPlayer win = new WindowsMediaPlayer();
+        WindowsMediaPlayer over = new WindowsMediaPlayer();
+
+        Random rnd = new Random();
+
+        PictureBox[] BlocosArray;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            musica.URL = "jogomusica.mp3";
+            win.URL = "Win.mp3";
+            over.URL = "Gameover.mp3";
+            ColocarBlocos();
+        }
+
+        private void JogoSetup()
+        {
+            musica.controls.play();
+
+            isGameOver = false;
+            pontuação = 0;
+            bolax = 8;
+            bolay = 8;
+            velocidade = 15;
+            score.Text = "Pontuação: " + pontuação;
+
+            Bola.Left = 730;//posicaox onde a bola começa
+            Bola.Top = 485;//posiçãoy onde a bola começa
+
+            jogador.Left = 650;//posiçãox onde jogador começa
+
+
+            relogio.Start();
+
+            foreach (Control x in this.Controls)// cor aleatoria dos blocos
+            {
+                if (x is PictureBox && (string)x.Tag == "Blocos")
+                {
+                    x.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                }
+            }
+        }
+
+        private void GameOver(string message)// contar pontuação
+        {
+
+            isGameOver = true;
+            relogio.Stop();
+            score.Text = "Pontuação: " + pontuação + " " + message;
+        }
+
+        private void ColocarBlocos()//colocação dos blocos automaticamente no inicio do jogo
+        {
+            BlocosArray = new PictureBox[66];
+
+            int a = 0;
+            int topo = 50;
+            int esq = 100;
+
+            for (int i = 0; i < BlocosArray.Length; i++)
+            {
+                BlocosArray[i] = new PictureBox();
+                BlocosArray[i].Height = 25;
+                BlocosArray[i].Width = 100;
+                BlocosArray[i].Tag = "Blocos";
+                BlocosArray[i].BackColor = Color.White;
+
+                if (a == 11)
+                {
+                    topo = topo + 50;
+                    esq = 100;
+                    a = 0;
+                }
+
+                if (a < 11)
+                {
+                    a++;
+                    BlocosArray[i].Left = esq;
+                    BlocosArray[i].Top = topo;
+                    this.Controls.Add(BlocosArray[i]);
+                    esq = esq + 110;
+                }
+            }
+
+            JogoSetup();
+        }
+
+        private void RemoverBlocos()// retirar blocos ao entrar em contato com a bola
+        {
+            foreach (PictureBox x in BlocosArray)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+
+        private void mainrelogioevent(object sender, EventArgs e)// velocidade da bola
+        {
+            score.Text = "Pontuação: " + pontuação;
+
+            if (iresq == true && jogador.Left > 0)
+            {
+                jogador.Left -= velocidade;
+            }
+            if (irdrt == true && jogador.Right < 1340)//limites de deslocação do jogador
+            {
+                jogador.Left += velocidade;
+            }
+
+            Bola.Left += bolax;
+            Bola.Top += bolay;
+
+            if (Bola.Left < 0 || Bola.Left > 1340)//limites de deslocação da bola
+            {
+                bolax = -bolax;
+            }
+            if (Bola.Top < 0)
+            {
+                bolay = -bolay;
+            }
+
+            if (Bola.Bounds.IntersectsWith(jogador.Bounds))
+            {
+                bolay = rnd.Next(8, 15) * -1;
+
+                if (bolax < 0)
+                {
+                    bolax = rnd.Next(8, 15) * -1;
+                }
+                else
+                {
+                    bolax = rnd.Next(8, 15);
+                }
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Blocos")//ao tocar nos blocos, abola desce e aumenta a pontuação
+                {
+                    if (Bola.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        pontuação += 1;
+                        bolay = -bolay;
+
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+            if (pontuação == 66)// Final do jogo
+            {
+                //Mostrar Mensagem de jogo concluido
+                GameOver("!Jogo Concluido! Prime a tecla Enter para jogar novamente.");
+                musica.controls.stop();
+                win.controls.play();
+            }
+
+            if (Bola.Top > 635)
+            {
+                //Mostrar Mensagem de Game Over
+                GameOver("Perdeste.Prime a tecla Enter para jogar novamente.");
+                musica.controls.stop();
+                over.controls.play();
+
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)// serve para quando as teclas são pressionadas realizar comandos
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                iresq = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                irdrt = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)//tecla aliviada
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                iresq = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                irdrt = false;
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RemoverBlocos();
+                ColocarBlocos();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'bDjogoDataSet.Pontuações'. Você pode movê-la ou removê-la conforme necessário.
+            //this.pontuaçõesTableAdapter.Fill(this.bDjogoDataSet.Pontuações);
+
+        }
+
+        private void pontuaçõesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pontuaçõesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bDjogoDataSet);
+
+        }
+
+        private void pontuaçõesBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
